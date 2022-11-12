7f9daebfbf06608ac53ed9d2b4c01f8aca4a35c3
Bernardo Martelli <bernardo.martelli@gmail.com>
2022-11-12 12:55:01 +0100

    make the table

--- week_7/src/components/table.tsx
+++ week_7/src/components/table.tsx
@@ -62,6 +62,7 @@ export default function GameTable(): JSX.Element {
       <div className="container" ref={cellPlaces}>
         {matrix()}
       </div>
+      <div className="table"></div>
       <Modal />
     </>
   );
